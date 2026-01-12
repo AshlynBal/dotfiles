0668ceb309894cc7eecca79b5976f3111410b9b3
Ashlyn Balicki <ashlynbal@proton.me>
2026-01-11 22:49:03 -0500

    Update to hyprland 0.53

---
+++ hyprland_scripts/float.sh
@@ -0,0 +1,6 @@
+# Toggle floating
+hyprctl dispatch togglefloating address:0x12345678
+# Resize to 50% screen size
+hyprctl dispatch resizewindowpixel exact 50% 50%,address:0x12345678
+# Center it
+hyprctl dispatch centerwindow address:0x12345678
